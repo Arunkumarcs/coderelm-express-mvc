29bc26dff402f33725610b7b60bbac1746bb56f5
Arunkumar <arun@auctionsoftware.com>
2019-04-02 12:29:00 +0530

    Update Watcher.js

--- core/Watcher.js
+++ core/Watcher.js
@@ -1,15 +1,37 @@
 class watcher {
     constructor() {
+        let self = this
         this._obj = {}
+
+        // default tracker
         this._tracker = function (name, obj, pro, oldVal, newVal) {
             console.log(`the property "${pro}" in  ${name}" is being changed to new value.`)
         }
-    }
 
-    set tracker(val) {
-        this._tracker = val
+        // default proxy obj
+        this._proxy = function (name) {
+            return {
+                // Setter
+                set(obj, prop, value) {
+                    self._tracker(
+                        name,
+                        obj,
+                        prop,
+                        obj[prop],
+                        value
+                    )
+                    obj[prop] = value
+                    return true
+                },
+                // Getter
+                get(obj, prop) {
+                    return obj[prop]
+                }
+            }
+        }
     }
-
+    
+    // create proxy for objects
     createProxy(element, name) {
         let self = this
         self[name] = element
@@ -18,24 +40,15 @@ class watcher {
             return name
         }
 
-        return new Proxy(element, {
-            // Setter
-            set(obj, prop, value) {
-                self._tracker(
-                    name,
-                    obj,
-                    prop,
-                    obj[prop],
-                    value
-                )
-                obj[prop] = value
-                return true
-            },
-            // Getter
-            get(obj, prop) {
-                return obj[prop]
-            }
-        })
+        return new Proxy(element, self._proxy(name))
+    }
+
+    // setters
+    set tracker(val) {
+        this._tracker = val
+    }
+    set proxy(val) {
+        this._proxy = val
     }
 }
 
@@ -56,8 +69,8 @@ class watcher {
 //     d3: "23qw3"
 // }
 
-// p1 = watchObj.proxy(p1, "p1")
-// p2 = watchObj.proxy(p2, "p2")
+// p1 = watchObj.createProxy(p1, "p1")
+// p2 = watchObj.createProxy(p2, "p2")
 
 // p1.d3 = "sadfsdf"
 // p2.d1 = "sadfsdf"
