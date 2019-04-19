3cb5da23762ebd86c2b9b2b2b00752cdd48eec42
Arunkumar <arunkumarcoderelm@gmail.com>
2019-04-19 09:04:40 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    name: 'expCodeRelm',
+    script: './bin/www',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    // max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
