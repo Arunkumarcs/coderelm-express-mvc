9eafb42d80fc984ba62b298d8c2077b439efe8ff
Arunkumar <arunkumarmessenger@gmail.com>
2019-06-12 12:01:19 +0530

    Update Provider.js

--- core/Coderelm/Provider.js
+++ core/Coderelm/Provider.js
@@ -4,6 +4,15 @@ class Provider extends App {
     constructor() {
         super();
     }
+
+    // static boot() {}
+    static beforeRoute() {}
+    static afterRoute() {}
+    static beforeServerCreate() {}
+    static afterServerCreate() {}
+    static beforeServerListen() {}
+    static afterServerListen() {}
+    // static end() {}
 }
 
 module.exports = Provider;
