f2de69e987713f47d03b58a7fbe160f9b88daac6
Arunkumar <arunkumarcoderelm@gmail.com>
2019-06-14 06:27:49 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'expCodeRelm',
-    script: './bin/www',
+    script: 'server.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     // args: 'one two',
